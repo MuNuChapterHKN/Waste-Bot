64a297932521778787492233e96c74261ab9f1c2
enzo-pellegrini <vincenzo.pellegrini.2001@gmail.com>
2021-04-29 00:09:58 +0200

    refractored and reformatted to prepare for the new user conversation handler

--- bot.py
+++ bot.py
@@ -3,25 +3,19 @@
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 import logging
-import ast
-import csv
 import os
 import re
 
-### Dictionary ###
-""" Creates an empty dictionary and updates it with the data contained in 'database.csv' if the file exists """
-D = {}
-if os.path.exists('database.csv'):
-	with open('database.csv','r') as file:
-		for line in csv.reader(file):
-			D[int(line[0])] = ast.literal_eval(line[1])
+# useless refactoring
+import db
 
 ### Enable logging ###
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)              # you will know when (and why) things don't work as expected
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)  # you will know when (and why) things don't work as expected
 logger = logging.getLogger(__name__)
 
 TOKEN = os.environ['TOKEN']
-#ADMIN_ID = os.environ['ADMIN_ID']
+# ADMIN_ID = os.environ['ADMIN_ID']
 
 PHOTOCODE, ANSWER = range(2)
 
@@ -48,143 +42,163 @@ no_keyboard = ReplyKeyboardRemove()
 
 ### Command handler functions ###
 """ Definition of the functions associated with the command handlers """
+
+
 def start(update: Update, _: CallbackContext) -> None:
-	"""Send a message when the command /start is issued."""
-	update.message.reply_text(text=welcome_msg, reply_markup=standard_keyboard)
+    """Send a message when the command /start is issued."""
+    update.message.reply_text(text=welcome_msg, reply_markup=standard_keyboard)
+    user = update.message.from_user
+    db.add_user(user.id, user.first_name, user.last_name, user.username)
 
-	"""Update dictionary."""
-	user = update.message.from_user
-	D[user.id] = {'firstname': user.first_name, 'lastname': user.last_name, 'username': user.username}
-
-	"""Update CSV file."""
-	file = open("database.csv", "w")
-	for key, value in D.items():
-		csv.writer(file).writerow([key, value])
-	file.close()
 
 def help_command(update: Update, _: CallbackContext) -> None:
-	"""Send a message when the command /help is issued."""
-	update.message.reply_text(text=help_msg, reply_markup=standard_keyboard)
+    """Send a message when the command /help is issued."""
+    update.message.reply_text(text=help_msg, reply_markup=standard_keyboard)
+
 
 def echo(update: Update, _: CallbackContext) -> None:
-	"""Echo the user message."""
-	update.message.reply_text(update.message.text, reply_markup=standard_keyboard)
+    """Echo the user message."""
+    update.message.reply_text(update.message.text, reply_markup=standard_keyboard)
+
 
 def unknown_command(update, context):
-	"""Send a message when an unknown command is issued."""
-	update.message.reply_text(text=unknown_command_msg, reply_markup=standard_keyboard)
+    """Send a message when an unknown command is issued."""
+    update.message.reply_text(text=unknown_command_msg, reply_markup=standard_keyboard)
+
 
 def unknown_message(update, context):
-	"""Send a message when an unknown message is issued."""
-	update.message.reply_text(text=unknown_message_msg, reply_markup=standard_keyboard)
+    """Send a message when an unknown message is issued."""
+    update.message.reply_text(text=unknown_message_msg, reply_markup=standard_keyboard)
+
 
 ### Conversation handler functions ###
 """ Definition of the functions associated with the conversation handlers """
+
+
 def ask_barcode(update: Update, _: CallbackContext) -> int:
-	"""Send a message when the command /code is issued asking for the barcode photo."""
-	#user = update.message.from_user
-	#logger.info("Modalita scelta of %s: %s", user.first_name, update.message.text)
-	update.message.reply_text(text=ask_barcode_msg, reply_markup=undo_keyboard)
-	return PHOTOCODE
+    """Send a message when the command /code is issued asking for the barcode photo."""
+    # user = update.message.from_user
+    # logger.info("Modalita scelta of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(text=ask_barcode_msg, reply_markup=undo_keyboard)
+    return PHOTOCODE
+
 
 def ask_photo(update: Update, _: CallbackContext) -> int:
-	"""Send a message when the command /photo is issued asking for the photo of the object."""
-	#user = update.message.from_user
-	#logger.info("Modalita scelta of %s: %s", user.first_name, update.message.text)
-	update.message.reply_text(text=ask_photo_msg, reply_markup=undo_keyboard)
-	return PHOTOCODE
+    """Send a message when the command /photo is issued asking for the photo of the object."""
+    # user = update.message.from_user
+    # logger.info("Modalita scelta of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(text=ask_photo_msg, reply_markup=undo_keyboard)
+    return PHOTOCODE
+
 
 def reply_barcode(update: Update, _: CallbackContext) -> int:
-	"""Sends a message in response to the barcode"""
-	#user = update.message.from_user
-	photo_file = update.message.photo[-1].get_file()
-	photo_file.download('user_code.jpg')
-	#logger.info("Codice of %s: %s", user.first_name, 'user_code.jpg')
-	update.message.reply_text(text=easter_barcode_msg, reply_markup=yesno_keyboard)
-	return ANSWER
+    """Sends a message in response to the barcode"""
+    # user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_code.jpg')
+    # logger.info("Codice of %s: %s", user.first_name, 'user_code.jpg')
+    update.message.reply_text(text=easter_barcode_msg, reply_markup=yesno_keyboard)
+    return ANSWER
+
 
 def reply_photo(update: Update, _: CallbackContext) -> int:
-	"""Sends a message in response to the photo"""
-	#user = update.message.from_user
-	photo_file = update.message.photo[-1].get_file()
-	photo_file.download('user_photo.jpg')
-	#logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-	update.message.reply_text(text=easter_photo_msg, reply_markup=yesno_keyboard)
-	return ANSWER
+    """Sends a message in response to the photo"""
+    # user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    # logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text(text=easter_photo_msg, reply_markup=yesno_keyboard)
+    return ANSWER
+
 
 def last_reply(update: Update, _: CallbackContext) -> int:
-	#user = update.message.from_user
-	#logger.info("User location of %s", user.first_name)
-	update.message.reply_text(text=waste_it_msg, reply_markup=standard_keyboard)
-	return ConversationHandler.END
+    # user = update.message.from_user
+    # logger.info("User location of %s", user.first_name)
+    update.message.reply_text(text=waste_it_msg, reply_markup=standard_keyboard)
+    return ConversationHandler.END
+
 
 def undo(update: Update, _: CallbackContext) -> int:
-	"""Cancel the barcode or photo request"""
-	#user = update.message.from_user
-	#logger.info("User %s canceled the conversation.", user.first_name)
-	update.message.reply_text(text=undo_msg, reply_markup=standard_keyboard)
-	return ConversationHandler.END
+    """Cancel the barcode or photo request"""
+    # user = update.message.from_user
+    # logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(text=undo_msg, reply_markup=standard_keyboard)
+    return ConversationHandler.END
+
 
 def invalid(update: Update, _: CallbackContext) -> None:
-	"""Send a message when the entered text is invalid."""
-	#user = update.message.from_user
-	#logger.info("User %s canceled the conversation.", user.first_name)
-	update.message.reply_text(text=invalid_msg, reply_markup=standard_keyboard)
-	return ConversationHandler.END
+    """Send a message when the entered text is invalid."""
+    # user = update.message.from_user
+    # logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(text=invalid_msg, reply_markup=standard_keyboard)
+    return ConversationHandler.END
+
 
 ### Main ###
 def main() -> None:
-	"""Start the bot."""
-	# Create the Updater and pass it your bot's token.
-	updater = Updater(TOKEN)
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(TOKEN)
 
-	# Get the dispatcher to register handlers
-	dispatcher = updater.dispatcher
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
 
-	# Command handlers
-	dispatcher.add_handler(CommandHandler('start', start))                                                                          # responds to the /start command
-	dispatcher.add_handler(CommandHandler('help', help_command))                                                                    # responds to the /help command
-	
-	# Conversation handlers
-	dispatcher.add_handler(
-		ConversationHandler(
-			entry_points=[CommandHandler('photo', ask_photo), MessageHandler(Filters.regex(re.compile(r'^foto$', re.IGNORECASE)), ask_photo)],                      # responds to the /photo command or to the "Foto" button
-			states={
-				PHOTOCODE: [MessageHandler(Filters.photo, reply_photo),                                                                                             # responds to the photo,
-							MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo),                                                           # responds toto the "Annulla" button 
-							MessageHandler(Filters.text & ~Filters.command, invalid)],                                                                              # responds to invalid messages
-				ANSWER: [MessageHandler(Filters.regex('^(Si|No)$'), last_reply),                                                                                    # responds to the "Si|No" buttons
-							MessageHandler(Filters.text & ~Filters.command, invalid)],                                                                              # responds to invalid messages
-			},
-			fallbacks=[CommandHandler('cancel', undo), MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo)],                               # responds to the /cancel command or to the "Annulla" button
-		)
-	)
+    # Command handlers
+    dispatcher.add_handler(CommandHandler('start', start))  # responds to the /start command
+    dispatcher.add_handler(CommandHandler('help', help_command))  # responds to the /help command
 
-	dispatcher.add_handler(
-		ConversationHandler(
-			entry_points=[CommandHandler('code', ask_barcode), MessageHandler(Filters.regex(re.compile(r'^codice a barre$', re.IGNORECASE)), ask_barcode)],         # responds to the /code command or to the "Codice a barre" button
-			states={
-				PHOTOCODE: [MessageHandler(Filters.photo, reply_barcode),                                                                                           # responds to the photo,
-							MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo),                                                           # responds toto the "Annulla" button 
-							MessageHandler(Filters.text & ~Filters.command, invalid)],                                                                              # responds to invalid messages
-				ANSWER: [MessageHandler(Filters.regex('^(Si|No)$'), last_reply),                                                                                    # responds to the "Si|No" buttons
-							MessageHandler(Filters.text & ~Filters.command, invalid)],                                                                              # responds to invalid messages
-			},
-			fallbacks=[CommandHandler('cancel', undo), MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo)],                               # responds to the /cancel command or to the "Annulla" button
-		)
-	)
+    # Conversation handlers
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler('photo', ask_photo),
+                          MessageHandler(Filters.regex(re.compile(r'^foto$', re.IGNORECASE)), ask_photo)],
+            # responds to the /photo command or to the "Foto" button
+            states={
+                PHOTOCODE: [MessageHandler(Filters.photo, reply_photo),  # responds to the photo,
+                            MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo),
+                            # responds toto the "Annulla" button
+                            MessageHandler(Filters.text & ~Filters.command, invalid)],  # responds to invalid messages
+                ANSWER: [MessageHandler(Filters.regex('^(Si|No)$'), last_reply),  # responds to the "Si|No" buttons
+                         MessageHandler(Filters.text & ~Filters.command, invalid)],  # responds to invalid messages
+            },
+            fallbacks=[CommandHandler('cancel', undo),
+                       MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo)],
+            # responds to the /cancel command or to the "Annulla" button
+        )
+    )
 
-	# Last handlers
-	dispatcher.add_handler(MessageHandler(Filters.command, unknown_command))                                                        # responds to any unknown command
-	dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, unknown_message))                                        # responds to any unknown message
+    dispatcher.add_handler(
+        ConversationHandler(
+            entry_points=[CommandHandler('code', ask_barcode),
+                          MessageHandler(Filters.regex(re.compile(r'^codice a barre$', re.IGNORECASE)), ask_barcode)],
+            # responds to the /code command or to the "Codice a barre" button
+            states={
+                PHOTOCODE: [MessageHandler(Filters.photo, reply_barcode),  # responds to the photo,
+                            MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo),
+                            # responds toto the "Annulla" button
+                            MessageHandler(Filters.text & ~Filters.command, invalid)],  # responds to invalid messages
+                ANSWER: [MessageHandler(Filters.regex('^(Si|No)$'), last_reply),  # responds to the "Si|No" buttons
+                         MessageHandler(Filters.text & ~Filters.command, invalid)],  # responds to invalid messages
+            },
+            fallbacks=[CommandHandler('cancel', undo),
+                       MessageHandler(Filters.regex(re.compile(r'^annulla$', re.IGNORECASE)), undo)],
+            # responds to the /cancel command or to the "Annulla" button
+        )
+    )
 
-	# Start the Bot
-	updater.start_polling()
+    # Last handlers
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown_command))  # responds to any unknown command
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, unknown_message))  # responds to any unknown message
 
-	# Run the bot until you press Ctrl-C or the process receives SIGINT,
-	# SIGTERM or SIGABRT. This should be used most of the time, since
-	# start_polling() is non-blocking and will stop the bot gracefully.
-	updater.idle()
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
 
 if __name__ == '__main__':
-	main()
+    main()
---
+++ db.py
@@ -0,0 +1,28 @@
+import os
+import ast
+import csv
+
+D = {}
+if os.path.exists('database.csv'):
+    with open('database.csv', 'r') as file:
+        for line in csv.reader(file):
+            D[int(line[0])] = ast.literal_eval(line[1])
+
+
+def _save_on_disk_():
+    global D
+    file = open("database.csv", "w")
+    for key, value in D.items():
+        csv.writer(file).writerow([key, value])
+
+    file.close()
+
+
+def is_user(userID: int) -> bool:
+    return userID in D.keys
+
+
+def add_user(userID: int, firstName: str, lastName: str, username: str):
+    global D
+    D[userID] = {'firstname': firstName, 'lastname': lastName, 'username': username}
+    _save_on_disk_()
